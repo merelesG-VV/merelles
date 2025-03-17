4d3c379ee4ee3537cf585400d1184f324d59d7c9
merelesG-VV <dhonavan.mereles@escola.pr.gov.br>
2025-03-17 08:24:07 -0300

    Add files via upload

---
+++ aluno dhonavan/script.js
@@ -0,0 +1,29 @@
+function calcular(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let operacao = document.getElementById('operacao'). value;
+    let resultado;
+
+    if(isNaN(num1) || isNaN(num2)){
+        resultado = "Erro! insira números válidos";
+    }else {
+        switch(operacao){
+            
+            case "+":
+                resultado = num1 + num2;
+                break; 
+            case "-":
+                resultado = num1 + num2;
+                break; 
+            case "*":
+                resultado = num1 + num2;
+                break; 
+            case "*":
+                resultado = num1 + num2;
+                break; 
+            case "/":
+                resultado = num2 !==0 ? num1/num2 : "Erro! Divisão por 0."
+                break; 
+        }
+    }
+    document.getElementById("resultado").textContent = resultado;
